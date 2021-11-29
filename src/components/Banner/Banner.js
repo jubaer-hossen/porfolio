79229072da66be0ba3190e1a935b65c28eb317e0
jubaer-hossen <jobaer1618@gmail.com>
2021-11-29 18:20:18 +0600

    added some animations

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,14 +8,14 @@ const Banner = () => {
                 <div className="row bg-img d-flex text-white align-items-center justify-content-start">
                     <div className="col-7 text-start ps-md-5 ms-md-5">
                         <h1 className="font-size font4">MD Jubaer Hossen</h1>
-                        <h2 className="font2">Web-developer</h2>
+                        <h2 className="font2 type-text">Web-developer</h2>
                         <a
                             target="_blank"
                             className="btn btn-primary font3"
                             // aria-current="page"
                             href="https://drive.google.com/u/0/uc?id=1xOgKe7tN1m_CK9HVFhD90cNorp-IhNly&export=download"
                         >
-                            Download resume
+                            <i class="fas fa-file-download"></i> Download resume
                         </a>
                     </div>
                 </div>
