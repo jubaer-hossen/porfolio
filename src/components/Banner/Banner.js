5671ed8138831334571efc04ca6d34129bf88957
jubaer-hossen <jobaer1618@gmail.com>
2021-12-05 07:21:48 +0600

    adede react typical

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typical from 'react-typical';
 import './Banner.css';
 
 const Banner = () => {
@@ -8,7 +9,26 @@ const Banner = () => {
                 <div className="row bg-img d-flex text-white align-items-center justify-content-start">
                     <div className="col-7 text-start ps-md-5 ms-md-5">
                         <h1 className="font-size font4">MD Jubaer Hossen</h1>
-                        <h2 className="font2 type-text">Web-developer</h2>
+                        <h2 className="font2">
+                            I'm a{' '}
+                            <span className="font1">
+                                <Typical
+                                    loop={Infinity}
+                                    wrapper="b"
+                                    steps={[
+                                        'Web-developer',
+                                        1000,
+                                        'React developer ',
+                                        1000,
+                                        'JavaScript developer ',
+                                        1000,
+                                        'Frontend developer ',
+                                        1000,
+                                    ]}
+                                />
+                            </span>
+                        </h2>
+
                         <a
                             target="_blank"
                             rel="noreferrer"
