81e70b624b517d71682454b0398a2f90c6f483fe
jubaer-hossen <jobaer1618@gmail.com>
2021-12-05 07:43:57 +0600

    added camera section

--- src/components/Project/Camera/Camera.js
+++ src/components/Project/Camera/Camera.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import DetailsNavbar from '../../DetailsNavbar/DetailsNavbar';
+import img1 from '../../../img/projects/Dream Camera.png';
 
 const Camera = () => {
     return (
-        <div className="h-view">
+        <div className="h-view text-center my-5">
             <DetailsNavbar></DetailsNavbar>
-            <p>cameras</p>
-            <h1>this is a camera</h1>
+            <h2 className="pt-5">Dream Camera shop screenshot</h2>
+            <div className="d-flex justify-content-center align-items-center">
+                <img
+                    className="w-50 text-center shadow-lg rounded border border-3 border-info"
+                    src={img1}
+                    alt=""
+                />
+            </div>
         </div>
     );
 };
