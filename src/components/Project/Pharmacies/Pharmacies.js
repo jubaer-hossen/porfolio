e42b40d33520287844ef96f857ddacbea319e017
jubaer-hossen <jobaer1618@gmail.com>
2021-12-05 07:47:37 +0600

    added some details in project sections

--- src/components/Project/Pharmacies/Pharmacies.js
+++ src/components/Project/Pharmacies/Pharmacies.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import DetailsNavbar from '../../DetailsNavbar/DetailsNavbar';
+import img1 from '../../../img/projects/React App.png';
 
 const Pharmacies = () => {
     return (
-        <div className="h-view">
+        <div className="h-view text-center my-5">
             <DetailsNavbar></DetailsNavbar>
-            <h1>this is pharmacy</h1>
+            <h2 className="pt-5">Local Pharmacies screenshot</h2>
+            <div className="d-flex justify-content-center align-items-center">
+                <img
+                    className="w-50 text-center shadow-lg rounded border border-3 border-info"
+                    src={img1}
+                    alt=""
+                />
+            </div>
         </div>
     );
 };
