c38e4d61b2a2cba51814826ca338683816a55e75
jubaer-hossen <jobaer1618@gmail.com>
2021-12-05 08:59:07 +0600

    added social link in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,42 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="bg-dark text-white text-center footer py-5">
-            <p>© Copyrighted by Jubaer Hossen.</p>
+            <div className="text-white social-icon">
+                <h1>Me on social</h1>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.facebook.com/Jubaer.Hossen.nur/"
+                >
+                    <i className="fab fa-facebook fs-3 me-2"></i>
+                </a>
+
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.instagram.com/md_jubaer_hossen_nur/"
+                >
+                    <i className="fab fa-instagram fs-3 me-2"></i>
+                </a>
+
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/jubaer-hossen/"
+                >
+                    <i className="fab fa-linkedin fs-3 me-2"></i>
+                </a>
+
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.youtube.com/channel/UCBcE8cdHHLGyOyyREH3M19w"
+                >
+                    <i className="fab fa-youtube fs-3"></i>
+                </a>
+            </div>
             <hr />
+            <p>© Copyrighted by Jubaer Hossen.</p>
         </div>
     );
 };
